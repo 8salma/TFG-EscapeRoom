2020662b39e54878320c9ad978164c20feafeebe
8salma <salmaperezup@gmail.com>
2024-01-04 13:04:20 +0100

    intento de pantalla de inicio

---
+++ Assets/Scripts/respiro.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respiro : MonoBehaviour
+{
+    public float breathingStrength = 0.1f; // Ajusta la fuerza de la "respiración"
+
+    void Update()
+    {
+        // Genera valores oscilantes usando Mathf.Sin y aplícalos a la posición de la cámara
+        float breathing = Mathf.Sin(Time.time) * breathingStrength;
+        transform.localPosition = new Vector3(breathing, 0.3301292f, 1.129231f); // Ajusta los ejes según tu preferencia
+    }
+}
